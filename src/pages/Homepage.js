2f371037c9b33d7810a5ee73b155b0e64367b13d
pedrodiogoneto <pedritoneto@gmail.com>
2019-10-21 19:15:17 +0200

    Add material ui

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,22 +3,40 @@ import styled from 'styled-components'
 
 import { connect } from 'react-redux';
 
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+
+import { GET_POSTS } from '../redux/actions/actions'
+
+
 const Homepage = (props) => {
+	const 
+
 
 	return(
-		<div>
+		<Wrapper>
 
-		</div>
+			<Fab color="secondary" aria-label="add">
+				<AddIcon />
+			</Fab>
+
+		</Wrapper>
 	)
 }
 
 const mapDispatchToProps = dispatch => ({
-
+	GET_POSTS: () => dispatch(GET_POSTS())
 })
 
 const mapStateToProps = state => ({
-
+	posts: state.posts,
 	
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Homepage)
+
+const Wrapper = styled.div`
+	display: flex;
+	align-self: flex-end;
+	flex-direction: row-reverse;
+`
