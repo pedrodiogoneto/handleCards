85a28f799836b91d47560f29a4d4974427ae6693
pedrodiogoneto <pedritoneto@gmail.com>
2019-10-23 17:18:24 +0200

    Fix post modal styles

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -11,12 +11,12 @@ export default function PostModal({ showModal, handleOnClose, handleOnSubmit, us
 	const [errorMessage, setErrorMessage] = useState('')
 	
 	useEffect(() => {
-		if(userAction === 'edit' && selectedPost) {
+		if(userAction === 'edit' && Object.keys(selectedPost).length !== 0) {
 			setTitle(selectedPost.title)
 			setDescription(selectedPost.description)
 			setImage(selectedPost.image)
 		}
-	}, [])
+	}, [userAction, selectedPost])
 
 	useEffect(() => {
 		return clearForm()
@@ -39,38 +39,44 @@ export default function PostModal({ showModal, handleOnClose, handleOnSubmit, us
 	}
     
 	return (
-		<div>
-			<Modal 
-				open={showModal}
-				onClose={handleOnClose}
-				disableAutoFocus={true}
-				style={{
-					position: 'absolute',
-					top: '25%',
-					left: '30%',
-					width: '40%',
-					userSelect: 'none'
-				}}>
-				<div style={{ 
-					backgroundColor: 'white',
-					padding: '10%',
-
-				}}>
-					<h2>Nueva tarjeta</h2>
-
-					<Input fullWidth required value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Título" />
-					<Input fullWidth required value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Descripción" />
-					<Input fullWidth value={image} onChange={(e) => setImage(e.target.value)} placeholder="Imagen (Url)" />
-                
-					{ errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage> }
-
+		<Modal open={showModal} onClose={handleOnClose} style={ModalStyles}>
+			<InnerModalWrapper>
+				<Title>Nueva tarjeta</Title>
+				<Input fullWidth required value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Título" />
+				<Input fullWidth required value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Descripción" />
+				<Input fullWidth value={image} onChange={(e) => setImage(e.target.value)} placeholder="Imagen (Url)" />
+				{ errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage> }
+				<ButtonWrapper>
 					<Button color="secondary" onClick={()=> handleSave()}>Anadir</Button>
-				</div>    
-			</Modal>
-		</div>
+				</ButtonWrapper>    
+			</InnerModalWrapper>    
+		</Modal>
 	)
+}
+
+const ModalStyles = {
+	position: 'absolute',
+	top: '25%',
+	left: '30%',
+	width: '40%',
+	userSelect: 'none'
 }
 
 const ErrorMessage = styled.p`
     color: red
+`
+
+const Title = styled.h2`
+    text-align: center
+`
+
+const ButtonWrapper = styled.div`
+	width: 100%;
+	text-align: center;
+	margin-top: 25px;
+`
+
+const InnerModalWrapper = styled.div`
+	background-color: white;
+	padding: 10%;
 `
