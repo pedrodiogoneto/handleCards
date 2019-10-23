a477ae6e76cac341ade3f9b1b1439de2848a1757
pedrodiogoneto <pedritoneto@gmail.com>
2019-10-23 17:37:03 +0200

    FIx initialReduxState

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,15 +1,14 @@
 const initialState = { 
 	userAction: '', 
 	posts: [
-		{id: 1, title: '1GOLF', description: 'kjhaslkdjhflkasjdhflkhjaslkdfhaskldjhfkajshdfklhasdlkfhkalsdhjfkjahsdfkhasdklfjh', url: 'https://www.mallorcadiario.com/fotos/1/408551_golf_thumb_708.jpg'},
-		{id: 2, title: '2GOLF', description: 'kjhaslkdjhflkasjdhflkhjaslkdfhaskldjhfkajshdfklhasdlkfhkalsdhjfkjahsdfkhasdklfjh', url: 'https://www.mallorcadiario.com/fotos/1/408551_golf_thumb_708.jpg'},
-		{id: 3, title: '3GOLF', description: 'kjhaslkdjhflkasjdhflkhjaslkdfhaskldjhfkajshdfklhasdlkfhkalsdhjfkjahsdfkhasdklfjh', url: 'https://www.mallorcadiario.com/fotos/1/408551_golf_thumb_708.jpg'}
+		{id: 1, title: 'GOLF', description: 'Golf is an amazing sport!', image: 'https://www.mallorcadiario.com/fotos/1/408551_golf_thumb_708.jpg'},
+		{id: 2, title: 'SURF', description: 'The biggest wave ever surfed was in nazaré, portugal', image: 'https://as.com/deportes_accion/imagenes/2019/04/30/surf/1556616678_839082_1556617141_noticia_normal.jpg'},
+		{id: 3, title: 'FOOTBALL', description: 'Also known as Ronaldo`s playground', image: 'https://zap.aeiou.pt/wp-content/uploads/2019/06/41d82ef9aa72abcc10169bf69db1411c-783x450.jpg'}
 	],   
 	error: '' 
 };
 
 export default function reduxReducer(state = initialState, action) {
-	console.log(action)
 	switch (action.type) {
 	case 'GET_POSTS': {
 		return {
@@ -48,7 +47,6 @@ export default function reduxReducer(state = initialState, action) {
 			posts: action.posts
 		};
 	}
-
 	default: {
 		return state;       
 	}   
